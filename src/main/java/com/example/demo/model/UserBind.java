9d718e7dfc543802639be0144151718e0c063a9a
fuhaixu001 <fuhaixu001@realsee.com>
2022-11-06 15:58:37 +0800

    增加了mapper和model

--- src/main/java/com/example/demo/model/UserBind.java
+++ src/main/java/com/example/demo/model/UserBind.java
@@ -1,15 +1,16 @@
 package com.example.demo.model;
 
 import lombok.Data;
+
 import java.util.Date;
 import java.util.Map;
 
 @Data
-public class User {
-    private Long id;
-    private String name;
-    private String avatar;
-    private String introduce;
+public class UserBind {
+    private  Long id;
+    private Long userId;
+    private Integer identityType;
+    private String identifier;
     private Integer status;
     private Map<String, Object> extension;
     private Date createTime;
